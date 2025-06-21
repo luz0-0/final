2561dd63eda67447708bf993f056613f46501b95
luz <maria.pucheta@est.fi.uncoma.edu.ar>
2025-06-21 01:06:03 -0300

    Update ResponsableV.php

--- ResponsableV.php
+++ ResponsableV.php
@@ -5,167 +5,169 @@ include_once 'Persona.php';
 
 class ResponsableV extends Persona {
 
-private $IDEmpleado;
-private $IDLicencia;
+    private $IDEmpleado;
+    private $IDLicencia;
+    private $mensaje;
 
-public function __construct(
-    $IDEmpleado = 0, 
-    $IDLicencia = 0
-) {
-    parent::__construct("", "");
-    $this->IDEmpleado = $IDEmpleado;
-    $this->IDLicencia = $IDLicencia;
-}
-
-public function getIDEmpleado() {
-    return $this->IDEmpleado;
-}
-
-public function setIDEmpleado($IDEmpleado) {
-    $this->IDEmpleado = $IDEmpleado;
-}
-public function getIDLicencia() {
-    return $this->IDLicencia;
-}
-public function setIDLicencia($IDLicencia) {
-    $this->IDLicencia = $IDLicencia;
-}
-
-public function cargarResponsableV($nombrePersona, $apellidoPersona, $IDEmpleado, $IDLicencia) {
-    $this->setNombrePersona($nombrePersona);
-    $this->setApellidoPersona($apellidoPersona);
-    $this->setIDEmpleado($IDEmpleado);
-    $this->setIDLicencia($IDLicencia);
-}
-
-
-public function insertarResponsableV() {
-    $base = new BaseDatos();
-    $resp = false;
-    $consultaResponsableV = " INSERT INTO ResponsableV(nombrePersona, apellidoPersona, IDEmpleado, IDLicencia) 
-    VALUES ('". 
-    $this->getNombrePersona() ."', '". 
-    $this->getApellidoPersona() ."', '". 
-    $this->getIDEmpleado() ."', '". 
-    $this->getIDLicencia() 
-    ."')";
-
-    if($base->IniciarBase()) {
-        if($base->IniciarBase($consultaResponsableV)) {
-            $resp = true;
-        } else {
-            $this->setMensaje($base->getERROR());
-        }
-    } else {
-        $this->setMensaje($base->getERROR());
+    public function __construct(
+        $IDEmpleado = 0, 
+        $IDLicencia = 0
+    ) {
+        parent::__construct("", "", "");
+        $this->IDEmpleado = $IDEmpleado;
+        $this->IDLicencia = $IDLicencia;
+        $this->mensaje = "";
     }
-    return $resp;
-}
 
-
-public function listarResponsableV($condicion = "") {
-    $arregloResponsables = null;
-    $base = new BaseDatos();
-    $consultaResponsableV = " SELECT * FROM ResponsableV ";
-
-    if ($condicion != "") {
-        $consultaResponsableV .= ' WHERE ' . $condicion;
+    public function getIDEmpleado() {
+        return $this->IDEmpleado;
     }
-    $consultaResponsableV .= " ORDER BY IDEmpleado ";
 
-    if ($base->Iniciar()) {
-        if ($base->Ejecutar($consultaResponsableV)) {
-            $arregloResponsables = array();
-            while ($row2 = $base->Registro()) {
-                $objResponsableV = new ResponsableV();
-                $objResponsableV->cargarResponsableV(
-                    $row2['nombrePersona'], 
-                    $row2['apellidoPersona'], 
-                    $row2['IDEmpleado'],
-                    $row2['IDLicencia']
-                );
-                array_push($arregloResponsables, $objResponsableV);
-            }
-        } else {
-            $this->setMensaje("ResponsableV->listar: ".$base->getError());
-        }
-    } else {
-        $this->setMensaje("ResponsableV->listar: ".$base->getError());
+    public function setIDEmpleado($IDEmpleado) {
+        $this->IDEmpleado = $IDEmpleado;
     }
-    return $arregloResponsables;
-}
 
-public function buscarResponsableV($IDEmpleado) {
-    $base = new BaseDatos();
-    $consultaResponsableV = " SELECT * FROM ResponsableV WHERE IDEmpleado = " . $IDEmpleado;
-    $resp = false;
+    public function getIDLicencia() {
+        return $this->IDLicencia;
+    }
 
-    if ($base->Iniciar()) {
-        if ($base->Ejecutar($consultaResponsableV)) {
-            if ($row2 = $base->Registro()) {
-                $this->cargarResponsableV(
-                    $row2['nombrePersona'], 
-                    $row2['apellidoPersona'], 
-                    $row2['IDEmpleado'],
-                    $row2['IDLicencia']
-                );
+    public function setIDLicencia($IDLicencia) {
+        $this->IDLicencia = $IDLicencia;
+    }
+
+    public function setMensaje($mensaje) {
+        $this->mensaje = $mensaje;
+    }
+
+    public function getMensaje() {
+        return $this->mensaje;
+    }
+
+    public function cargarResponsableV($nombrePersona, $apellidoPersona, $IDEmpleado, $IDLicencia) {
+        $this->setNombrePersona($nombrePersona);
+        $this->setApellidoPersona($apellidoPersona);
+        $this->setIDEmpleado($IDEmpleado);
+        $this->setIDLicencia($IDLicencia);
+    }
+
+    public function insertarResponsableV() {
+        $base = new BaseDatos();
+        $resp = false;
+        $consultaResponsableV = "INSERT INTO ResponsableV(nombrePersona, apellidoPersona, IDEmpleado, IDLicencia) 
+            VALUES (
+                '" . $this->getNombrePersona() . "', 
+                '" . $this->getApellidoPersona() . "', 
+                '" . $this->getIDEmpleado() . "', 
+                '" . $this->getIDLicencia() . "'
+            )";
+        if ($base->IniciarBase()) {
+            if ($base->EjecutarBase($consultaResponsableV)) {
                 $resp = true;
+            } else {
+                $this->setMensaje($base->getERROR());
             }
         } else {
-            $this->setMensaje($base->getError());
+            $this->setMensaje($base->getERROR());
         }
-    } else {
-        $this->setMensaje($base->getError());
+        return $resp;
     }
-    return $resp;
-}
 
-public function eliminarResponsableV() {
-    $base = new BaseDatos();
-    $resp = false;
-    $consultaResponsableV = "DELETE FROM ResponsableV WHERE IDEmpleado = " . $this->getIDEmpleado();
+    public function listarResponsableV($condicion = "") {
+        $arregloResponsables = null;
+        $base = new BaseDatos();
+        $consultaResponsableV = "SELECT * FROM ResponsableV";
+        if ($condicion != "") {
+            $consultaResponsableV .= ' WHERE ' . $condicion;
+        }
+        $consultaResponsableV .= " ORDER BY IDEmpleado";
+        if ($base->IniciarBase()) {
+            if ($base->EjecutarBase($consultaResponsableV)) {
+                $arregloResponsables = array();
+                while ($row2 = $base->Registro()) {
+                    $objResponsableV = new ResponsableV();
+                    $objResponsableV->cargarResponsableV(
+                        $row2['nombrePersona'], 
+                        $row2['apellidoPersona'], 
+                        $row2['IDEmpleado'],
+                        $row2['IDLicencia']
+                    );
+                    array_push($arregloResponsables, $objResponsableV);
+                }
+            } else {
+                $this->setMensaje("ResponsableV->listar: " . $base->getERROR());
+            }
+        } else {
+            $this->setMensaje("ResponsableV->listar: " . $base->getERROR());
+        }
+        return $arregloResponsables;
+    }
 
-    if ($base->IniciarBase()) {
-        if ($base->IniciarBase($consultaResponsableV)) {
-            $resp = true;
+    public function buscarResponsableV($IDEmpleado) {
+        $base = new BaseDatos();
+        $consultaResponsableV = "SELECT * FROM ResponsableV WHERE IDEmpleado = " . intval($IDEmpleado);
+        $resp = false;
+        if ($base->IniciarBase()) {
+            if ($base->EjecutarBase($consultaResponsableV)) {
+                if ($row2 = $base->Registro()) {
+                    $this->cargarResponsableV(
+                        $row2['nombrePersona'], 
+                        $row2['apellidoPersona'], 
+                        $row2['IDEmpleado'],
+                        $row2['IDLicencia']
+                    );
+                    $resp = true;
+                }
+            } else {
+                $this->setMensaje($base->getERROR());
+            }
         } else {
             $this->setMensaje($base->getERROR());
         }
-    } else {
-        $this->setMensaje($base->getERROR());
+        return $resp;
     }
-    return $resp;
-}
 
-public function modificarResponsableV() {
-    $base = new BaseDatos();
-    $resp = false;
-    $consultaResponsableV = " UPDATE ResponsableV SET 
-        nombrePersona = '" . $this->getNombrePersona() . "', 
-        apellidoPersona = '" . $this->getApellidoPersona() . "', 
-        IDLicencia = '" . $this->getIDLicencia() . "' 
-        WHERE IDEmpleado = " . $this->getIDEmpleado();
-
-    if ($base->IniciarBase()) {
-        if ($base->IniciarBase($consultaResponsableV)) {
-            $resp = true;
+    public function eliminarResponsableV() {
+        $base = new BaseDatos();
+        $resp = false;
+        $consultaResponsableV = "DELETE FROM ResponsableV WHERE IDEmpleado = " . intval($this->getIDEmpleado());
+        if ($base->IniciarBase()) {
+            if ($base->EjecutarBase($consultaResponsableV)) {
+                $resp = true;
+            } else {
+                $this->setMensaje($base->getERROR());
+            }
         } else {
             $this->setMensaje($base->getERROR());
         }
-    } else {
-        $this->setMensaje($base->getERROR());
+        return $resp;
     }
-    return $resp;
-}
 
+    public function modificarResponsableV() {
+        $base = new BaseDatos();
+        $resp = false;
+        $consultaResponsableV = "UPDATE ResponsableV SET 
+            nombrePersona = '" . $this->getNombrePersona() . "', 
+            apellidoPersona = '" . $this->getApellidoPersona() . "', 
+            IDLicencia = '" . $this->getIDLicencia() . "' 
+            WHERE IDEmpleado = " . intval($this->getIDEmpleado());
+        if ($base->IniciarBase()) {
+            if ($base->EjecutarBase($consultaResponsableV)) {
+                $resp = true;
+            } else {
+                $this->setMensaje($base->getERROR());
+            }
+        } else {
+            $this->setMensaje($base->getERROR());
+        }
+        return $resp;
+    }
 
-public function __toString() {
-    return "ResponsableV: " . 
-    $this->getNombrePersona() . " " . 
-    $this->getApellidoPersona() . ", " . 
-    "IDEmpleado: " . $this->getIDEmpleado() . ", " . 
-    "IDLicencia: " . $this->getIDLicencia();
-}
-
+    public function __toString() {
+        return "ResponsableV: " . 
+            $this->getNombrePersona() . " " . 
+            $this->getApellidoPersona() . ", " . 
+            "IDEmpleado: " . $this->getIDEmpleado() . ", " . 
+            "IDLicencia: " . $this->getIDLicencia();
+    }
 
 }
